e8dbee001d44c54492b13ca9b58fbf3a1bb3566d
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2018-12-06 16:15:41 -0500

    Update set-pipeline.sh
    renames global yml file to params.yml

--- pipelines/backup/set-pipeline.sh
+++ pipelines/backup/set-pipeline.sh
@@ -12,7 +12,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # set pipeline variables in order of most generic to most specific 
 # to take advantage of cascading variable overrides
 fly -t concourse set-pipeline $DIR/pipeline.yml \
-  -l $DIR/global.yml \
+  -l $DIR/params.yml \
   -l $DIR/environments/$BBR_ENVIRONMENT/params.yml \
   -l $DIR/regions/$BBR_REGION/params.yml \
   -l $DIR/regions/$BBR_REGION/$BBR_ENVIRONMENT/params.yml
